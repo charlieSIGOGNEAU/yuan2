730fa2607f4ea54f8511e5262ae16ed6b8a2cbc0
charlieSIGOGNEAU <charliesigogneau@gmail.com>
2025-11-08 19:56:38 +0100

    Configure frontend to use HTTPS API with domain name

--- docs/js/app/config.js
+++ docs/js/app/config.js
@@ -1,6 +1,6 @@
 export const ServerConfig = {
-	HTTP_BASE: 'http://79.137.35.177:3000/api/v1/',
-	WS_URL: 'ws://79.137.35.177:3000/cable',
+	HTTP_BASE: 'https://yuan-game.com/api/v1/',
+	WS_URL: 'wss://yuan-game.com/cable',
 	GOOGLE_CLIENT_ID : '929416731081-oseq2arg4ofd9tpbs4ku9s6so0tl0028.apps.googleusercontent.com'
 
 };
