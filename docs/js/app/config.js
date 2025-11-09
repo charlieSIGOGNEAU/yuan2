b27463e3f6d6451460d3b34694435d69476ef745
charlieSIGOGNEAU <charliesigogneau@gmail.com>
2025-11-09 17:02:28 +0100

    aucun changement

--- docs/js/app/config.js
+++ docs/js/app/config.js
@@ -1,13 +1,15 @@
+// vertion test pour le smarphone mais echec
+// export const ServerConfig = {
+// 	HTTP_BASE: 'http://192.168.1.50:3001/api/v1/',
+//     WS_URL: 'ws://192.168.1.50:3001/cable',
+// 	GOOGLE_CLIENT_ID : '929416731081-oseq2arg4ofd9tpbs4ku9s6so0tl0028.apps.googleusercontent.com'
+// };
+
+// vertion fonctionelle  sans uttiliser le smarphone
 export const ServerConfig = {
 	HTTP_BASE: 'http://localhost:3001/api/v1/',
 	WS_URL: 'ws://localhost:3001/cable',
 	GOOGLE_CLIENT_ID : '929416731081-oseq2arg4ofd9tpbs4ku9s6so0tl0028.apps.googleusercontent.com'
-
 };
-// export const ServerConfig = {
-// 	HTTP_BASE: 'http://localhost:3000/api/v1/',
-// 	WS_URL: 'ws://localhost:3000/cable',
-// 	GOOGLE_CLIENT_ID : '1035919176567-gs6oqpls9lhfq14dp0b489o6i4urhp8j.apps.googleusercontent.com'
 
-// };
 
