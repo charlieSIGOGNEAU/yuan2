e292cf3d0df98bd36e1b167758c0513da90202f2
charlieSIGOGNEAU <charliesigogneau@gmail.com>
2025-11-10 16:29:55 +0100

    retour au test fronte eberger chez moi

--- docs/js/app/config.js
+++ docs/js/app/config.js
@@ -1,6 +1,21 @@
-export const ServerConfig = {
-	HTTP_BASE: 'https://yuan-game.com/api/v1/',
-	WS_URL: 'wss://yuan-game.com/cable',
-	GOOGLE_CLIENT_ID : '1035919176567-8lmu5ncnm9un9m04kqksjaeqvth8a0dv.apps.googleusercontent.com'
+// vertion test pour le smarphone mais echec
+// export const ServerConfig = {
+// 	HTTP_BASE: 'http://192.168.1.50:3001/api/v1/',
+//     WS_URL: 'ws://192.168.1.50:3001/cable',
+// 	GOOGLE_CLIENT_ID : '929416731081-oseq2arg4ofd9tpbs4ku9s6so0tl0028.apps.googleusercontent.com'
+// };
 
+// vertion fonctionelle  sans uttiliser le smarphone
+export const ServerConfig = {
+	HTTP_BASE: 'http://localhost:3001/api/v1/',
+	WS_URL: 'ws://localhost:3001/cable',
+	GOOGLE_CLIENT_ID : '929416731081-oseq2arg4ofd9tpbs4ku9s6so0tl0028.apps.googleusercontent.com'
 };
+
+// version fonctionelle pour le deploiement
+// export const ServerConfig = {
+// 	HTTP_BASE: 'https://yuan-game.com/api/v1/',
+// 	WS_URL: 'wss://yuan-game.com/cable',
+// 	GOOGLE_CLIENT_ID : '1035919176567-8lmu5ncnm9un9m04kqksjaeqvth8a0dv.apps.googleusercontent.com'
+
+// };
